ef7c54604ad62b608fe162af90f805a6c6e79bb5
LJ S.A <lvjhn.d@gmail.com>
2024-08-04 05:26:11 +0800

    General Update

--- scripts/precomp_vecs.sh
+++ scripts/precomp_vecs.sh
@@ -1,3 +1,6 @@
-cpulimit -l 40 -- python3 -m utils.precomp_vecs "kaggle|amananandrai|AG News Classification Dataset|ag-news.csv" "text" &
-cpulimit -l 40 -- python3 -m utils.precomp_vecs "kaggle|lakshmi25npathi|IMDB Dataset|IMDB Dataset.csv" "review" &
-cpulimit -l 40 -- python3 -m utils.precomp_vecs "github|aaroncarlfernandex|Philippine Fake News Corpus|Philippine Fake News Corpus.csv" "Headline|Content"
+kaggle/amananandrai/AG News Classification Dataset/all.csv/Class Index
+kaggle/banuprakashv/News Article Classification Dataset/all.csv/category
+kaggle/dabayondharchowdhury/News Text/all.csv/category
+kaggle/khoshbayani/World News Category/all.csv/label
+kaggle/rmisra/News Category Dataset/all.csv/category
+kaggle/setseries/News Category Dataset/all.csv/category
